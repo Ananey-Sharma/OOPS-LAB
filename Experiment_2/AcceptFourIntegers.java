b46f365b4dce7d9c6a15e4709fe39c47704c0657
Ananey Sharma <ananeysharma7@gmail.com>
2026-02-23 14:36:46 +0530

    Updated AcceptFourIntegers program

--- Experiment_2/AcceptFourIntegers.java
+++ Experiment_2/AcceptFourIntegers.java
@@ -1,9 +1,32 @@
 package Experiment_2;
 
+import java.util.Scanner;
+
 public class AcceptFourIntegers {
     public static void main(String[] args) {
 
-    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter first integer: ");
+    int a = sc.nextInt();
+
+    System.out.print("Enter second integer: ");
+    int b = sc.nextInt();
+
+    System.out.print("Enter third integer: ");
+    int c = sc.nextInt();
+
+    System.out.print("Enter fourth integer: ");
+    int d = sc.nextInt();
+
+    if (a == b && b == c && c == d) {
+        System.out.println("All integers are equal.");
+    } else {
+        System.out.println("The integers are not all equal.");
     }
 
+    sc.close();
+    
 }
+}
+ 
